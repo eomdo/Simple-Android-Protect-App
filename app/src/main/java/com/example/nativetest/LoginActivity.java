ad93273a89efc7fc0b218e55f79b61064c6573be
sihaku <senpiary@gmail.com>
2023-07-25 16:58:03 +0900

    code update

--- app/src/main/java/com/example/nativetest/LoginActivity.java
+++ app/src/main/java/com/example/nativetest/LoginActivity.java
@@ -2,6 +2,7 @@ package com.example.nativetest;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
---
+++ app/src/main/java/com/example/nativetest/VulnDeeplink.java
@@ -0,0 +1,14 @@
+package com.example.nativetest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VulnDeeplink extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vuln_deeplink);
+    }
+}
